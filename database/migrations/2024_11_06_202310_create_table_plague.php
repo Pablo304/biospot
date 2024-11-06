b99dc80cd5f9554501c97fb863b4b1a865e1ce0b
paiva <lucas.paiva@ae3tecnologia.com.br>
2024-11-06 17:08:23 -0400

    refatorando tabela de pragas

---
+++ database/migrations/2024_11_06_202310_create_table_plague.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plagues', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('plague_type_id');
+            $table->unsignedBigInteger('suspect_id');
+            $table->unsignedBigInteger('process_info_id');
+            $table->unsignedBigInteger('plague_status_id');
+
+            $table->foreign('plague_type_id')->references('id')->on('plague_types');
+            $table->foreign('suspect_id')->references('id')->on('suspects');
+            $table->foreign('process_info_id')->references('id')->on('process_infos');
+            $table->foreign('plague_status_id')->references('id')->on('plague_statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plagues');
+    }
+};
