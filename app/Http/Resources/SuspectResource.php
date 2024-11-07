34e4725c855404f3fcaf2b42a81ae85215a031f1
pablo <pablomiranda304@gmail.com>
2024-11-06 20:41:56 -0400

    [FEAT]: correcao de rota

--- app/Http/Resources/SuspectResource.php
+++ app/Http/Resources/SuspectResource.php
@@ -14,6 +14,7 @@ class SuspectResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        dd($this);
         return [
             'id' => $this->id,
             'complaint' => [
