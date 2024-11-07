6114f637867e5472c911d9274d10b19fd5f4f01a
pablo <pablomiranda304@gmail.com>
2024-11-06 20:21:10 -0400

    [FEAT]: correcao de rota

--- routes/api.php
+++ routes/api.php
@@ -37,12 +37,12 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     });
 
     Route::prefix('plague')->group(function () {
+        Route::get('/plague-types', [PlagueTypeController::class, 'index'])->name('plague.types');
         Route::get('/', [PlagueController::class, 'index'])->name('plague.index');
         Route::get('/{plague}', [PlagueController::class, 'show'])->name('plague.show');
         Route::post('/', [PlagueController::class, 'store'])->name('plague.store');
         Route::post('/{plague}/resolve', [PlagueController::class, 'resolve'])->name('plague.store');
         Route::get('/plague-status', [PlagueStatusController::class, 'index']);
-        Route::get('/plague-types', [PlagueTypeController::class, 'index'])->name('plague.types');
     });
     Route::get('/status', [StatusController::class, 'index'])->name('status.index');
 });
